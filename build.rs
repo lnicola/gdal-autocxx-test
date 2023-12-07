71ced69532f3fa6571ccf6f9476bd001c18a904b
Laurențiu Nicola <lnicola@dend.ro>
2023-12-07 20:21:43 +0200

    Show how to set the lib path

--- build.rs
+++ build.rs
@@ -2,12 +2,14 @@ fn main() -> miette::Result<()> {
     let path = std::path::PathBuf::from("src"); // include path
     let mut b = autocxx_build::Builder::new("src/lib.rs", &[&path]).build()?;
     // This assumes all your C++ bindings are in main.rs
-    b.flag_if_supported("-std=c++14").compile("autocxx-demo"); // arbitrary library name, pick anything
+    b.flag_if_supported("-std=c++14")
+        .compile("autocxx-gdal-test"); // arbitrary library name, pick anything
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/utils.h");
 
     // Add instructions to link to any C++ libraries you need.
     println!("cargo:rustc-link-lib=dylib=gdal");
+    // println!("cargo:rustc-link-search=.");
 
     Ok(())
 }
